c81190e375beaf366b2755e51ef864ec0cb69381
sogo.ohta <sogo.ohta@eure.jp>
2020-05-25 02:18:48 +0900

    change server port

--- library-backend.js
+++ library-backend.js
@@ -138,6 +138,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen().then(({ url }) => {
+server.listen(4001).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
